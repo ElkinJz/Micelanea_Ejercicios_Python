516544cf2e50015c28d20dc37d666a5dd6c27b16
ElkinJz <131708305+ElkinJz@users.noreply.github.com>
2023-05-02 22:53:43 -0500

    Add files via upload

---
+++ 3.1. ELEVAR UN NUMERO AL CUADRADO.py
@@ -0,0 +1,12 @@
+print("ELEVAR NUMERO AL CUADRADO 2")
+
+#entrada
+a = int(input("Digite el numero a elevar al acuadrado: "))
+
+#Proceso 
+
+elevarcuadrado = pow(a, 2)
+
+#Salida
+
+print("El numero elevado es: ", elevarcuadrado)
